d7e063f12af17ccdab80de87a739060647e13116
ivanevacic <ivanevacicweb@gmail.com>
2018-03-14 15:11:49 +0100

    Added UI class and started working on functions

--- appES6.js
+++ appES6.js
@@ -1,1 +1,38 @@
 //es6
+
+class Book{
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI{
+    // class functions
+    addBookToList(book){
+        //  get list with books html elements
+        const list = document.getElementById('book-list');
+        //  create tr element
+        const row = document.createElement('tr');
+        //insert columns(rows) into tr
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">X<a></td>
+        `;
+        //  append that row to the book list
+        list.appendChild(row);
+        
+    }
+    showAlert(message, className){
+
+    }
+    deleteBook(target){
+
+    }
+    clearFields(){
+
+    }
+}
